3616c67460a1b8bd4c5082aa2501ada79b4a12e7
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-11-17 21:02:24 -0300

    probando los deletes

--- timeTracker/models.py
+++ timeTracker/models.py
@@ -4,33 +4,37 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Tarea(models.Model):
-	nombre = models.CharField(max_length=200)
-	def __str__(self):
-		return str(self.nombre)
-
 class Proyecto(models.Model):
 	nombre= models.CharField(max_length=200)
-	tareas = models.ForeignKey(Tarea, blank=True, null=True, on_delete=models.CASCADE)
 	def __str__(self):
-		return str(self.nombre)
+		return self.nombre
 
-
-class Horas(models.Model):
-	cantidad = models.IntegerField(validators=[MinValueValidator(0)])
-	fecha = models.DateTimeField(auto_now_add=True, blank=False)
-	tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+class Tarea(models.Model):
+	nombre = models.CharField(max_length=200)
+	proyecto = models.ForeignKey(Proyecto, blank= True, null=True, on_delete=models.SET_NULL)
 	def __str__(self):
-		return str(self.cantidad)
+		return self.nombre
 
 
 class Desarrollador(models.Model):
 	nombre = models.CharField(max_length=200)
 	apellido= models.CharField(max_length=200)
 	edad = models.IntegerField(validators=[MinValueValidator(0)])
-	horas = models.ForeignKey(Horas, blank=True, null=True, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.nombre+' '+self.apellido
+
+
+
+class Horas(models.Model):
+	cantidad = models.IntegerField(validators=[MinValueValidator(0)])
+	fecha = models.DateTimeField(auto_now_add=True, blank=False)
+	tarea = models.ForeignKey(Tarea, on_delete=models.SET_NULL, blank= True, null=True)
+	desarrollador = models.ForeignKey(Desarrollador, on_delete=models.CASCADE, blank= True, null=True)
+	def __str__(self):
+		return 'Proyecto= ' + str(self.tarea.proyecto)+\
+		', \nTarea= ' + str(self.tarea)+ \
+		', \nDesarrollador = ' + str(self.desarrollador) +\
+		', \nCantidad= ' + str(self.cantidad)+' hs'
 
 
 
