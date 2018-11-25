d8c235a78b484b6b5747179abd2a63edc3cd3815
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-11-25 01:19:38 -0300

    base html

--- timeTracker/views.py
+++ timeTracker/views.py
@@ -3,20 +3,27 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 import simplejson as json
+from datetime import datetime
 
 from .models import Proyecto
 from .models import Tarea
 from .models import Desarrollador
+from .models import Horas
 
 
-def tareasPorProyecto():
-	dic = {}
+
+tareasPorProyecto = None
+
+def fillGlobalDictionary():
+	global tareasPorProyecto
+	tareasPorProyecto = {}
 	for proyecto in Proyecto.objects.all():
 		tareas = []
 		for tarea in proyecto.tarea_set.all():
 			tareas.append(tarea.nombre)
-		dic[proyecto.nombre] = tareas
-	return dic;
+		tareasPorProyecto[proyecto.nombre] = tareas
+	return tareasPorProyecto;
+
 
 
 def index(request):
@@ -27,8 +34,9 @@ def index(request):
 def detail(request, desarrollador_id):
 	desarrollador = get_object_or_404(Desarrollador, pk=desarrollador_id)
 	proyectos = Proyecto.objects.all()
-	dic = tareasPorProyecto()
-	context = {'desarrollador': desarrollador,'proyectos':proyectos, 'tareasPorProyecto':json.dumps(dic)}
+	fillGlobalDictionary()
+	global tareasPorProyecto
+	context = {'desarrollador': desarrollador,'proyectos':proyectos, 'tareasPorProyecto':json.dumps(tareasPorProyecto)}
 	return render(request, 'timeTracker/detail.html',context)
 
 def add(request, desarrollador_id):
@@ -36,18 +44,18 @@ def add(request, desarrollador_id):
 	
 	cantidad = int(request.POST['cantidad'])
 
-	print request.POST['proyecto']
-	#print Proyecto.objects.all().get(pk=request.POST['proyecto'])	
-	
-	
-	print cantidad
-	dic = tareasPorProyecto()
-	tarea_index = int(request.POST['tarea'])
-	tareas = dic.get(proyecto)
-	tarea = tareas[tarea_index]
-	print tarea
+	proyecto =  request.POST['proyecto']
 
-	#print desarrollador.horas_set.create( cantidad = cantidad, proyecto = proyecto )
+	tarea_index = int(request.POST['tarea'])
+	global tareasPorProyecto
+	tarea_name = tareasPorProyecto.get(proyecto)[tarea_index]
+	tarea =  get_object_or_404(Tarea, nombre=tarea_name)
+
+	fecha = request.POST['fecha']
+	fecha = datetime.strptime(fecha, '%Y-%m-%d')
+
+	#Guardo las horas
+	desarrollador.horas_set.create( desarrollador = desarrollador, tarea = tarea, cantidad = cantidad, fecha = fecha)
 	return HttpResponseRedirect(reverse('timeTracker:detail', args=(desarrollador.id,)))
    
 
